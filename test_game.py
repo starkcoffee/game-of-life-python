827cb126f933ca4e509352917aa5ac723515f152
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-08-08 21:46:51 +1000

    Start test for dead cells

--- test_game.py
+++ test_game.py
@@ -1,4 +1,8 @@
+import pytest
 from collections import namedtuple
+
+A = 0
+D = 1
 
 class Game:
 
@@ -14,3 +18,13 @@ def test_game_is_initialised_with_board():
 def test_game_returns_board_after_step():
   game = Game([[]])
   assert(game.step()) == [[]]
+
+@pytest.mark.parametrize(
+  "test_input, expected",
+  [
+    ([[D]], [[D]])
+  ]
+)
+def test_dead_cell_with_no_neighbours_stays_dead(test_input, expected):
+  assert(Game(test_input).step()) == expected
+  
