8e531abf59fa9fa2055e6552c67ed0e54a0ec5b7
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-08-08 21:18:25 +1000

    Add step method

--- test_game.py
+++ test_game.py
@@ -5,6 +5,12 @@ class Game:
   def __init__(self, board):
     self.board = board
 
+  def step(self):
+    return self.board
+
 def test_game_is_initialised_with_board():
-    assert(Game([[]]).board) == [[]]
+  assert(Game([[]]).board) == [[]]
   
+def test_game_returns_board_after_step():
+  game = Game([[]])
+  assert(game.step()) == [[]]
