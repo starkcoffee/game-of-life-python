30840bbf9e1dab2643df56ceb122aaa846b68e49
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-08-08 22:53:55 +1000

    Start on test living cells die

--- test_game.py
+++ test_game.py
@@ -10,6 +10,12 @@ class Game:
     self.board = board
 
   def step(self):
+    # for each cell, if it is alive, if it has fewer than 2 neighbours, then make it a dead cell
+    for row in self.board:
+      for col, cell in enumerate(row):
+        if cell == A:
+          row[col] = D
+
     return self.board
 
 def test_game_is_initialised_with_board():
@@ -29,4 +35,12 @@ def test_game_returns_board_after_step():
 def test_dead_cell_with_no_neighbours_stays_dead(test_input, expected):
   assert(Game(test_input).step()) == expected
   
+@pytest.mark.parametrize(
+  "test_input, expected",
+  [
+    ([[A]], [[D]]),
+  ]
+)
 
+def test_live_cells_with_fewer_than_two_neighbours_die(test_input, expected):
+  assert(Game(test_input).step()) == expected
