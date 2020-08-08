4cb9029ac9ef186c9a8f8e0ee9ac378ca66e2c75
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-08-09 01:51:59 +1000

    zomg fix mutability problem

--- test_game.py
+++ test_game.py
@@ -9,22 +9,33 @@ class Game:
   def __init__(self, board):
     self.board = board
 
+  def num_rows(self):
+    return len(self.board)
+
+  def num_cols(self):
+    return len(self.board[0])
+
   def list_neighbours(self, cell_cords):
     x, y = cell_cords
     neighbour_coords = [ (x+i, y+j) for i, j in [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)] ]
     
     return [ self.board[j][i] for i,j in neighbour_coords 
-      if i >= 0 and i < len(self.board[0]) and  j >=0 and j < len(self.board) 
+      if i >= 0 and i < self.num_cols() and j >=0 and j < self.num_rows()
     ]
 
   def cell_should_die(self, cell_coords):
     return len([ neighbour for neighbour in self.list_neighbours(cell_coords) if neighbour == A ]) < 2
 
   def step(self):
+    cells_to_be_killed = []
+
     for y, row in enumerate(self.board):
       for x, cell in enumerate(row):
         if cell == A and self.cell_should_die((x, y)):
-          row[x] = D
+          cells_to_be_killed.append((x,y))
+
+    for x,y in cells_to_be_killed:
+      self.board[y][x] = D  
 
     return self.board
 
@@ -43,6 +54,11 @@ class Game:
 )
 def test_returns_neighbours(board, cell_coords, expected):
   assert(Game(board).list_neighbours(cell_coords)) == expected
+
+def test_cell_should_die_if_has_less_than_two_neighbours():
+  assert(Game([[A, A, A]]).cell_should_die((0,0))) == True
+  assert(Game([[A, A, A]]).cell_should_die((1,0))) == False
+  assert(Game([[A, A, A]]).cell_should_die((2,0))) == True
 
 def test_game_is_initialised_with_board():
   assert(Game([[]]).board) == [[]]
@@ -66,9 +82,10 @@ def test_dead_cell_with_no_neighbours_stays_dead(test_input, expected):
   [
     ([[A]], [[D]]),
     ([[A,A]], [[D, D]]),
+    ([[A],[A]], [[D],[D]]),
     ([[A,A,A]], [[D,A,D]]),
+    ([[A],[A],[A]], [[D],[A],[D]]),
   ]
 )
-@pytest.mark.skip
 def test_live_cells_with_fewer_than_two_neighbours_die(test_input, expected):
   assert(Game(test_input).step()) == expected
